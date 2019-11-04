d165b037bd3449e9ae28d8773b0e92c9a3998df5
Kenneth-joseph <josephochieng812@gmail.com>
2019-11-04 10:30:03 +0300

    added several staff on my what we do section:toggle function on the firt two images and text on the first one

---
+++ app.js
@@ -0,0 +1,18 @@
+$(Document).ready(function(){
+$("#image1,#para1").on("click",function(){
+    $("#image1,#para1").toggleClass("d-none");
+});
+
+$("#image2,#para2").on("click",function(){
+    $("#image2,#para2").toggleClass("d-none");
+
+})
+});
+ 
+
+
+function send(){
+    var nameR= document.getElementById("name").value;
+    var emailE = document.getElementById("email").value;
+    alert("your name is "+ nameR + "your emal is " + emailE);
+}
